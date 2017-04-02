efb8292e6f0623e7c7aef00704f3dfce092694c5
DESKTOP-T85G3V9\eleonora <leo1994448@gmail.com>
2017-04-02 19:15:55 +0100

    Comunicando obsrvador con elementos observables

--- MasterDetailApp/src/main/java/es/ulpgc/eite/clean/mvp/dbmasterdetail/app/ObserverMasterDetail.java
+++ MasterDetailApp/src/main/java/es/ulpgc/eite/clean/mvp/dbmasterdetail/app/ObserverMasterDetail.java
@@ -1,9 +1,14 @@
 package es.ulpgc.eite.clean.mvp.dbmasterdetail.app;
 
+import android.util.Log;
+
 import java.util.Observable;
 import java.util.Observer;
 
+import es.ulpgc.eite.clean.mvp.dbmasterdetail.detail.DetailPresenter;
 import es.ulpgc.eite.clean.mvp.dbmasterdetail.master.MasterPresenter;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by eleonora on 02/04/2017.
@@ -12,8 +17,15 @@ import es.ulpgc.eite.clean.mvp.dbmasterdetail.master.MasterPresenter;
 public class ObserverMasterDetail implements Observer {
     @Override
     public void update(Observable o, Object arg) {
-        if(arg instanceof MasterPresenter){
+        if(arg instanceof MasterPresenter ){
           MasterPresenter mp=(MasterPresenter)arg;
-            System.out.println("master has changed");
+            Log.d(TAG,"master has changed");
         }
-}}
+        if(arg instanceof DetailPresenter){
+            DetailPresenter dp=(DetailPresenter)arg;
+           Log.d(TAG,"detail has changed");
+        }
+
+    }
+
+}
